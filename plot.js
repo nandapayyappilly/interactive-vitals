eb84c67c4ef40e80d1e0cdc46ddb8c593ef2bed0
Jiya Makhija <jmakhija@ucsd.edu>
2025-05-11 15:11:10 -0700

    Update plot.js

--- plot.js
+++ plot.js
@@ -1,100 +1,93 @@
-const svg = d3.select("svg"),
-      width = +svg.attr("width") - 100,
-      height = +svg.attr("height") - 100,
-      margin = { top: 50, right: 50, bottom: 50, left: 50 };
+const margin = { top: 50, right: 40, bottom: 50, left: 60 },
+      width = 900 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
 
-const chart = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+const svg = d3.select("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-const xScale = d3.scaleLinear().domain([0, 1]).range([0, width]);
-const yScale = d3.scaleLinear().range([height, 0]);
+const x = d3.scaleLinear().domain([0, 1]).range([0, width]);
+const y = d3.scaleLinear().range([height, 0]);
 
-const color = d3.scaleOrdinal(d3.schemeTableau10);
+const xAxis = d3.axisBottom(x).tickFormat(d3.format(".0%"));
+const yAxis = d3.axisLeft(y);
 
-const xAxis = chart.append("g").attr("transform", `translate(0,${height})`);
-const yAxis = chart.append("g");
+svg.append("g").attr("transform", `translate(0,${height})`).attr("class", "x-axis");
+svg.append("g").attr("class", "y-axis");
 
-const vitalSelect = d3.select("body").insert("select", ":first-child")
-  .attr("id", "vital-select")
-  .selectAll("option")
-  .data(["map", "hr", "spo2"])
-  .enter().append("option")
-  .text(d => d.toUpperCase());
+const line = d3.line()
+    .x(d => x(d.norm_time))
+    .y(d => y(d.mean))
+    .curve(d3.curveMonotoneX);
 
-const groupSelect = d3.select("body").insert("select", ":first-child")
-  .attr("id", "group-select")
-  .selectAll("option")
-  .data(["optype", "emop"])
-  .enter().append("option")
-  .text(d => d === "optype" ? "Surgery Type" : "Emergency Flag");
+const area = d3.area()
+    .x(d => x(d.norm_time))
+    .y0(d => y(d.mean - d.sd))
+    .y1(d => y(d.mean + d.sd))
+    .curve(d3.curveMonotoneX);
 
-d3.csv("data/long_surgery_vitals.csv", d3.autoType).then(data => {
-  update("map", "optype");
+const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-  d3.select("#vital-select").on("change", function() {
-    const vital = this.value;
-    const group = document.getElementById("group-select").value;
-    update(vital, group);
-  });
+const vitalSelect = d3.select("body").insert("select", ":first-child").attr("id", "vitalSelect");
+const groupSelect = d3.select("body").insert("select", ":first-child").attr("id", "groupSelect");
 
-  d3.select("#group-select").on("change", function() {
-    const group = this.value;
-    const vital = document.getElementById("vital-select").value;
-    update(vital, group);
-  });
+d3.csv("cleaned_surgery_long.csv", d3.autoType).then(data => {
+  const vitals = [...new Set(data.map(d => d.signal))];
+  const groups = ["optype", "emop"];
 
-  function update(vital, group) {
-    const nested = d3.groups(
-      data.filter(d => d.signal === vital && !isNaN(d.value)),
-      d => d[group]
-    );
+  vitalSelect.selectAll("option")
+    .data(vitals).enter().append("option")
+    .text(d => d).attr("value", d => d);
 
-    const line = d3.line()
-      .x(d => xScale(d.norm_time))
-      .y(d => yScale(d.mean));
+  groupSelect.selectAll("option")
+    .data(groups).enter().append("option")
+    .text(d => d === "optype" ? "Surgery Type" : "Emergency Status").attr("value", d => d);
 
-    const area = d3.area()
-      .x(d => xScale(d.norm_time))
-      .y0(d => yScale(d.mean - d.stdev))
-      .y1(d => yScale(d.mean + d.stdev));
+  function updateChart() {
+    const selectedVital = vitalSelect.property("value");
+    const selectedGroup = groupSelect.property("value");
+
+    const filtered = data.filter(d => d.signal === selectedVital);
+    const nested = d3.groups(filtered, d => d[selectedGroup]);
 
     const summary = nested.map(([key, values]) => {
-      const binned = d3.bin()
-        .value(d => d.norm_time)
-        .thresholds(100)(values);
-      const points = binned.map(bin => {
-        const mean = d3.mean(bin, d => d.value);
-        const stdev = d3.deviation(bin, d => d.value) || 0;
+      const byTime = d3.groups(values, d => d.norm_time).map(([t, pts]) => {
+        const v = pts.map(p => p.value);
         return {
-          norm_time: (bin.x0 + bin.x1) / 2,
-          mean,
-          stdev
+          norm_time: +t,
+          mean: d3.mean(v),
+          sd: d3.deviation(v)
         };
-      }).filter(d => !isNaN(d.mean));
-      return { key, points };
+      });
+      return { key, values: byTime.sort((a, b) => a.norm_time - b.norm_time) };
     });
 
-    yScale.domain([d3.min(summary, s => d3.min(s.points, d => d.mean - d.stdev)),
-                   d3.max(summary, s => d3.max(s.points, d => d.mean + d.stdev))]);
+    y.domain([d3.min(summary, s => d3.min(s.values, d => d.mean - d.sd)),
+              d3.max(summary, s => d3.max(s.values, d => d.mean + d.sd))]);
 
-    xAxis.call(d3.axisBottom(xScale).tickFormat(d => `${Math.round(d * 100)}%`));
-    yAxis.call(d3.axisLeft(yScale));
+    svg.select(".x-axis").call(xAxis);
+    svg.select(".y-axis").call(yAxis);
 
-    chart.selectAll(".group").remove();
-
-    const groups = chart.selectAll(".group")
-      .data(summary)
-      .enter().append("g")
-      .attr("class", "group");
-
-    groups.append("path")
-      .attr("fill", (d, i) => color(i))
-      .attr("opacity", 0.2)
-      .attr("d", d => area(d.points));
-
-    groups.append("path")
-      .attr("stroke", (d, i) => color(i))
+    svg.selectAll(".line").data(summary, d => d.key)
+      .join("path")
+      .attr("class", "line")
       .attr("fill", "none")
+      .attr("stroke", d => color(d.key))
       .attr("stroke-width", 2)
-      .attr("d", d => line(d.points));
+      .attr("d", d => line(d.values));
+
+    svg.selectAll(".area").data(summary, d => d.key)
+      .join("path")
+      .attr("class", "area")
+      .attr("fill", d => color(d.key))
+      .attr("fill-opacity", 0.2)
+      .attr("stroke", "none")
+      .attr("d", d => area(d.values));
   }
+
+  vitalSelect.on("change", updateChart);
+  groupSelect.on("change", updateChart);
+  updateChart();
 });
